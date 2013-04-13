f9f7775a0cd60398e1234b5a15a1db27bcaeaf28
tpltnt <tpltnt.github@dropcut.net>
2013-04-13 21:18:35 +0200

    address parsing tests added

--- tests/test_baseframe.py
+++ tests/test_baseframe.py
@@ -3,5 +3,21 @@ sys.path.append('../py-hdlc')
 import pytest
 from hdlc import HDLCBaseFrame
 
+
 def test_plain_init():
     foo = HDLCBaseFrame()
+
+
+def test_parse_address_string_input(baseframe):
+    with pytest.raises(TypeError):
+        baseframe.parse_address('test')
+
+
+def test_parse_address_float_input(baseframe):
+    with pytest.raises(TypeError):
+        baseframe.parse_address(2.3)
+
+
+def test_parse_address_bytearray_input(baseframe):
+    with pytest.raises(TypeError):
+        baseframe.parse_address(bytearray(1))
