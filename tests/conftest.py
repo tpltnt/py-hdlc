a362338c5f4fe15559108c5f42c9c7eb6dc21941
tpltnt <tpltnt.github@dropcut.net>
2013-04-16 19:19:58 +0200

    base packet fixture added

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,4 +6,11 @@ from hdlc import HDLCBaseFrame
 
 @pytest.fixture(scope="module")
 def baseframe():
+    """create a HDLCBaseFrame-object"""
     return HDLCBaseFrame()
+
+
+@pytest.fixture(scope="module")
+def unstrippedemptyunicastbasechunk():
+    """create a minimal basechunk with valid flags"""
+    return b'\x7e\xff\x00\x00\x7e'
