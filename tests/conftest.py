343ee43a25b515d27e22c7edf739a227d709ad9e
tpltnt <tpltnt.github@dropcut.net>
2013-04-17 18:13:38 +0200

    baseframe fixture removed since it seems to return always the same object to tests -> meh

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,10 +4,10 @@ import pytest
 from hdlc import HDLCBaseFrame
 
 
-@pytest.fixture(scope="module")
-def baseframe():
-    """create a HDLCBaseFrame-object"""
-    return HDLCBaseFrame()
+#@pytest.fixture(scope="module")
+#def baseframe():
+#    """create a HDLCBaseFrame-object"""
+#    return HDLCBaseFrame()
 
 
 @pytest.fixture(scope="module")
