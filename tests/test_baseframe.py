ac5cc30732576f20aa052e7e08aa1fa772b3e5e6
tpltnt <tpltnt.github@dropcut.net>
2013-04-17 18:14:58 +0200

    all tests migrated away from baseframe-fixture

--- tests/test_baseframe.py
+++ tests/test_baseframe.py
@@ -8,63 +8,74 @@ def test_plain_init():
     foo = HDLCBaseFrame()
 
 
-def test_parse_address_string_input(baseframe):
+def test_parse_address_string_input():
+    baseframe = HDLCBaseFrame()
     with pytest.raises(TypeError):
         baseframe.parse_address('test')
 
 
-def test_parse_address_float_input(baseframe):
+def test_parse_address_float_input():
+    baseframe = HDLCBaseFrame()
     with pytest.raises(TypeError):
         baseframe.parse_address(2.3)
 
 
-def test_parse_address_bytearray_input(baseframe):
+def test_parse_address_bytearray_input():
+    baseframe = HDLCBaseFrame()
     with pytest.raises(TypeError):
         baseframe.parse_address(bytearray(1))
 
 
-def test_parse_address_too_short_input1(baseframe):
+def test_parse_address_too_short_input1():
     """stripped input: just two zero-bytes"""
+    baseframe = HDLCBaseFrame()
     with pytest.raises(ValueError):
         baseframe.parse_address(bytes(2))
 
 
-def test_parse_address_too_short_input2(baseframe):
+def test_parse_address_too_short_input2():
     """unstripped input: just four zero-bytes"""
+    baseframe = HDLCBaseFrame()
     with pytest.raises(ValueError):
         baseframe.parse_address(bytes(4))
 
 
-def test_parse_address_too_short_input3(baseframe):
+def test_parse_address_too_short_input3():
     """unstripped input, 4 bytes, valid opening and closing flags"""
+    baseframe = HDLCBaseFrame()
     with pytest.raises(ValueError):
         baseframe.parse_address(b'\x7e\x00\x00\x7e')
 
 
-def test_parse_address_too_short_input4(baseframe):
+def test_parse_address_too_short_input4():
     """unstripped input, 4 bytes, invalid opening flags"""
+    baseframe = HDLCBaseFrame()
     with pytest.raises(ValueError):
         baseframe.parse_address(b'\x23\x00\x00\x7e')
 
 
-def test_parse_address_too_short_input5(baseframe):
+def test_parse_address_too_short_input5():
     """unstripped input, 4 bytes, invalid closing flags"""
+    baseframe = HDLCBaseFrame()
     with pytest.raises(ValueError):
         baseframe.parse_address(b'\x7e\x00\x00\x42')
 
 
-def test_is_allstation1(baseframe, chunk1):
+def test_is_allstation1(chunk1):
     """test frame generated with unicast/allstation address for having one"""
+    baseframe = HDLCBaseFrame()
     baseframe.parse_address(chunk1)
     assert baseframe.is_allstation() is True
 
 
-def test_is_allstation2(baseframe):
+def test_is_allstation2():
     """vanilla object should not have an allstation address assigned"""
+    baseframe = HDLCBaseFrame()
     assert baseframe.is_allstation() is False
 
 
-def test_is_allstation3(baseframe, chunk2):
+def test_is_allstation3(chunk2):
     """"given a no-station address, testing is_allstation should yield False"""
+    baseframe = HDLCBaseFrame()
     baseframe.parse_address(chunk2)
     assert baseframe.is_allstation() is False
