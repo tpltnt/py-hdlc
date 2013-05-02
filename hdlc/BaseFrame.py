e6bec79f16ca39980af778924f9e0e8b69e91c29
tpltnt <tpltnt.github@dropcut.net>
2013-05-02 23:34:50 +0200

    get_receive ... removed since its not generic

--- hdlc/BaseFrame.py
+++ hdlc/BaseFrame.py
@@ -177,25 +177,3 @@ class BaseFrame(object):
             return True
         else:
             return False
-
-    def get_receive_sequence_number(self):
-        """
-        Extract transmit receive sequence number from control field.
-
-        :returns: int -- receive sequence number
-        :raises: ValueError
-
-        """
-        if 1 == len(self.__control):
-            # 8 bit control field
-            ctrlbits = self.__control[0]
-            # masking up to (expected) 8 bits
-            if 8 < ctrlbits.bit_length():
-                raise ValueError("internal control field longer than 8 bits")
-            # mask out all but lowest (rightmost) 3 bits
-            mask = ~0xf8
-            seqbits = ctrlbits & mask
-            return int(seqbits)
-        else:
-            # 16 bit control field
-            raise NotImplementedError("handling 16bit control fields not implemented (yet)")
