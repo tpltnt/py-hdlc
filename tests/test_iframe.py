914e1f3954902d9413a05652d70a917ee7fda9f8
tpltnt <tpltnt.github@dropcut.net>
2013-05-03 21:12:05 +0200

    iframe tests cleaned up

--- tests/test_iframe.py
+++ tests/test_iframe.py
@@ -10,226 +10,217 @@ def test_plain_init():
 
 def test_parse_address1():
     """test for rejecting strings"""
-    baseframe = IFrame()
+    iframe = IFrame()
     with pytest.raises(TypeError):
-        baseframe.parse_address('test')
+        iframe.parse_address('test')
 
 
 def test_parse_address2():
     """test for rejecting float input"""
-    baseframe = IFrame()
+    iframe = IFrame()
     with pytest.raises(TypeError):
-        baseframe.parse_address(2.3)
+        iframe.parse_address(2.3)
 
 
 def test_parse_address3():
     """test for too short bytearray """
-    baseframe = IFrame()
+    iframe = IFrame()
     with pytest.raises(TypeError):
-        baseframe.parse_address(bytearray(1))
+        iframe.parse_address(bytearray(1))
 
 
 def test_parse_address4():
     """stripped input: just two zero-bytes"""
-    baseframe = IFrame()
+    iframe = IFrame()
     with pytest.raises(ValueError):
-        baseframe.parse_address(bytes(2))
+        iframe.parse_address(bytes(2))
 
 
 #def test_parse_address_too_short_input2():
 #    """unstripped input: just four zero-bytes"""
-#    baseframe = IFrame()
+#    iframe = IFrame()
 #    with pytest.raises(ValueError):
-#        baseframe.parse_address(bytes(4))
+#        iframe.parse_address(bytes(4))
 
 
 def test_parse_address5():
     """unstripped input, 4 bytes, valid opening and closing flags"""
-    baseframe = IFrame()
+    iframe = IFrame()
     with pytest.raises(ValueError):
-        baseframe.parse_address(b'\x7e\x00\x00\x7e')
+        iframe.parse_address(b'\x7e\x00\x00\x7e')
 
 
 def test_parse_address6():
     """unstripped input, 4 bytes, invalid opening flags"""
-    baseframe = IFrame()
+    iframe = IFrame()
     with pytest.raises(ValueError):
-        baseframe.parse_address(b'\x23\x00\x00\x7e')
+        iframe.parse_address(b'\x23\x00\x00\x7e')
 
 
 def test_parse_address7():
     """unstripped input, 4 bytes, invalid closing flags"""
-    baseframe = IFrame()
+    iframe = IFrame()
     with pytest.raises(ValueError):
-        baseframe.parse_address(b'\x7e\x00\x00\x42')
+        iframe.parse_address(b'\x7e\x00\x00\x42')
 
 
-def test_parse_address8(chunk3):
-    """unstripped input, 5 bytes, 2byte address
-    baseframe only allows 8 bits
-    """
-    baseframe = IFrame()
-    with pytest.raises(ValueError):
-        baseframe.parse_address(chunk3)
+#def test_parse_address8(chunk3):
+#    """unstripped input, 5 bytes, 2byte address
+#    iframe only allows 8 bits
+#    """
+#    iframe = IFrame()
+#    with pytest.raises(ValueError):
+#        iframe.parse_address(chunk3)
 
 
 def test_parse_address9(chunk4):
     """parse I-frame control field with 1 byte address"""
-    baseframe = IFrame()
-    baseframe.parse_address(chunk4)
+    iframe = IFrame()
+    iframe.parse_address(chunk4)
 
 
 def test_is_allstation1(chunk1):
     """test frame generated with unicast/allstation address for having one"""
-    baseframe = IFrame()
-    baseframe.parse_address(chunk1)
-    assert baseframe.is_allstation() is True
+    iframe = IFrame()
+    iframe.parse_address(chunk1)
+    assert iframe.is_allstation() is True
 
 
 def test_is_allstation2():
     """vanilla object should not have an allstation address assigned"""
-    baseframe = IFrame()
-    assert baseframe.is_allstation() is False
+    iframe = IFrame()
+    assert iframe.is_allstation() is False
 
 
 def test_is_allstation3(chunk2):
     """"given a no-station address, testing is_allstation should yield False"""
-    baseframe = IFrame()
-    baseframe.parse_address(chunk2)
-    assert baseframe.is_allstation() is False
+    iframe = IFrame()
+    iframe.parse_address(chunk2)
+    assert iframe.is_allstation() is False
 
 
 def test_is_nostation1():
     """'no station' should be the default address for a base frame."""
-    baseframe = IFrame()
-    assert baseframe.is_nostation() is True
+    iframe = IFrame()
+    assert iframe.is_nostation() is True
 
 
 def test_is_nostation2(chunk1):
     """parsing a all-station address should return False on is_nostation()"""
-    baseframe = IFrame()
-    baseframe.parse_address(chunk1)
-    assert baseframe.is_nostation() is False
+    iframe = IFrame()
+    iframe.parse_address(chunk1)
+    assert iframe.is_nostation() is False
 
 
 def test_is_nostation3(chunk2):
     """parsing a no-station address should return True in is_nostation()"""
-    baseframe = IFrame()
-    baseframe.parse_address(chunk2)
-    assert baseframe.is_nostation() is True
+    iframe = IFrame()
+    iframe.parse_address(chunk2)
+    assert iframe.is_nostation() is True
 
 
 def test_set_address1():
     """setting an arbitary 8bit address with clear MSB should work."""
-    baseframe = IFrame()
-    baseframe.set_address(bytes([42]))
+    iframe = IFrame()
+    iframe.set_address(bytes([42]))
 
 
 def test_set_address2():
     """giving int as address should fail"""
-    baseframe = IFrame()
+    iframe = IFrame()
     with pytest.raises(TypeError):
-        baseframe.set_address(42)
+        iframe.set_address(42)
 
 
 def test_set_address3():
     """giving string as address should fail"""
-    baseframe = IFrame()
+    iframe = IFrame()
     with pytest.raises(TypeError):
-        baseframe.set_address('23')
+        iframe.set_address('23')
 
 
 def test_set_address4():
     """setting 'all station' should work"""
-    baseframe = IFrame()
-    baseframe.set_address(bytes([255]))
-    assert baseframe.is_allstation() is True
-
-
-def test_set_address5():
-    """Setting a 16bit address on base frame should fail.
-    The ISO standard only allows 8bit for baseframe."""
-    baseframe = IFrame()
-    with pytest.raises(ValueError):
-        baseframe.set_address(bytes([23, 42]))
+    iframe = IFrame()
+    iframe.set_address(bytes([255]))
+    assert iframe.is_allstation() is True
 
 
 def test_get_address0():
-    """setting and retrieving an 8bit address on a baseframe."""
-    baseframe = IFrame()
-    baseframe.set_address(bytes([42]))
-    assert bytes([42]) == baseframe.get_address()
+    """setting and retrieving an 8bit address on a iframe."""
+    iframe = IFrame()
+    iframe.set_address(bytes([42]))
+    assert bytes([42]) == iframe.get_address()
 
 
 def test_get_address1():
     """test for returning bytes()"""
-    baseframe = IFrame()
-    assert isinstance(baseframe.get_address(), bytes)
+    iframe = IFrame()
+    assert isinstance(iframe.get_address(), bytes)
 
 
 def test_set_control0():
     """test for passing a bytes instance."""
-    baseframe = IFrame()
-    baseframe.set_control(bytes([42]))
+    iframe = IFrame()
+    iframe.set_control(bytes([42]))
 
 
 def test_set_control1():
     """test for passing Bytes array."""
-    baseframe = IFrame()
+    iframe = IFrame()
     with pytest.raises(TypeError):
-        baseframe.set_control(bytearray(1))
+        iframe.set_control(bytearray(1))
 
 
 def test_set_control2():
-    """test for setting 24 bits"""
-    baseframe = IFrame()
-    with pytest.raises(ValueError):
-        baseframe.set_control(bytes([5, 23, 42]))
+    """test for setting 24 control bits"""
+    iframe = IFrame()
+    iframe.set_control(bytes([5, 23, 42]))
 
 
 def test_get_control0():
     """test for retrieving set control field."""
-    baseframe = IFrame()
-    baseframe.set_control(bytes([42]))
-    assert bytes([42]) == baseframe.get_control()
+    iframe = IFrame()
+    iframe.set_control(bytes([42]))
+    assert bytes([42]) == iframe.get_control()
 
 
 def test_get_control1():
     """test for retrieving correct type"""
-    baseframe = IFrame()
-    assert isinstance(baseframe.get_control(), bytes)
+    iframe = IFrame()
+    assert isinstance(iframe.get_control(), bytes)
 
 
 def test_is_IFrame0():
     """set I-frame bit and control for it."""
-    baseframe = IFrame()
-    baseframe.set_control(bytes([42]))
-    assert baseframe.is_IFrame() is True
+    iframe = IFrame()
+    iframe.set_control(bytes([42]))
+    assert iframe.is_IFrame() is True
 
 
 def test_is_IFrame1():
     """test non-I-frame for being one."""
-    baseframe = IFrame()
-    baseframe.set_control(bytes([128]))
-    assert baseframe.is_IFrame() is False
+    iframe = IFrame()
+    iframe.set_control(bytes([128]))
+    assert iframe.is_IFrame() is False
 
 
 def test_get_receive_sequence_number0():
     """test for extracting max number"""
-    baseframe = IFrame()
+    iframe = IFrame()
     # 119 = 0x77 = 01110111
-    baseframe.set_control(bytes([119]))
-    assert 7 == baseframe.get_receive_sequence_number()
+    iframe.set_control(bytes([119]))
+    assert 7 == iframe.get_receive_sequence_number()
 
 
 def test_get_receive_sequence_number1():
     """test for extracting min. number"""
-    baseframe = IFrame()
-    baseframe.set_control(bytes([112]))
-    assert 0 == baseframe.get_receive_sequence_number()
+    iframe = IFrame()
+    iframe.set_control(bytes([112]))
+    assert 0 == iframe.get_receive_sequence_number()
 
 
 def test_get_receive_sequence_number2():
     """test for correct return type"""
-    baseframe = IFrame()
-    assert isinstance(baseframe.get_receive_sequence_number(), int)
+    iframe = IFrame()
+    assert isinstance(iframe.get_receive_sequence_number(), int)
